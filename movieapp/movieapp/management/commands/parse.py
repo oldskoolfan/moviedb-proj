d000f0808096c53df9a984bd7ff95202758d7354
Andrew Harris <harris.1305@gmail.com>
2017-03-16 10:31:50 -0400

    lint parse.py

--- movieapp/movieapp/management/commands/parse.py
+++ movieapp/movieapp/management/commands/parse.py
@@ -1,18 +1,23 @@
-import sys
+"""
+manage.py command for parsing genres.list file
+"""
 from datetime import datetime
 from imdbcrawler.worker import Worker
-from pprint import pprint
-from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		self.stdout.write(self.style.SUCCESS('start: %s' % datetime.now()))
-		try:
-			myWork = Worker()
-			numberOfMovies = myWork.parseList()
-			self.stdout.write(self.style.SUCCESS('end: %s' % datetime.now()))
-			msg = "Number of movies: %s" % numberOfMovies
-			self.stdout.write(self.style.SUCCESS(msg))
-		except Exception as e:
-			raise CommandError(e)
+    """ our command class, extending django BaseCommand """
+    
+    def handle(self, *args, **options):
+        """ create worker, parse list file """
+        
+        self.stdout.write(self.style.SUCCESS('start: %s' % datetime.now()))
+        try:
+            myWork = Worker()
+            numberOfMovies = myWork.parseList()
+            self.stdout.write(self.style.SUCCESS('end: %s' % datetime.now()))
+            msg = "Number of movies: %s" % numberOfMovies
+            self.stdout.write(self.style.SUCCESS(msg))
+        except Exception as e:
+            raise CommandError(e)
+            
